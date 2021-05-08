c98c25431fcb2e3903de8f80361361eb46fae6d7
TeaSkittle <tea@pop-os.localdomain>
2021-05-08 14:38:04 -0600

    Edit main.d

--- main.d
+++ main.d
@@ -2,66 +2,59 @@
 import std.stdio;
 import std.string;
 import std.conv;
+import std.file;
 
-int main(string[] args){
+int main( string[] args ){
   int id, opt;
-  if (args.length < 2) {
-    printUsage();
+  File file;
+  if ( args.length < 2 ) {
+    writeln( "usage: todo <option> [task]" );
     return 1;
   } else {
-    opt = stringToInt(args[1]);
-    parseOptions(opt,id, args);
-  }
-  return 0;
+    opt = stringToInt( args[1] );
+    parseOptions( opt, id, args, file );
+  } return 0;
 }
 
-void parseOptions(int opt, int id, string[] args){
-  switch(opt){
-  case 0:  // list
-    writeln("list");
-    break;
-  case 1:  // add
-    break;
-  case 2:  // done
-    id = to!int(args[2]);
-    break;
-  case 3:  // delete( may not use )
-    break;
-  default: // Error
-    writeln("Unkown argument");
-    break;
-  }
-}
-
-int stringToInt(string s){
-  switch(s){
-  case "list":
-    return 0;
-    break;
-  case "add":
-    return 1;
-    break;
-  case "done":
-    return 2;
-    break;
-  case "delete":
-    return 3;
-    break;
-  default:
-    return -1;
-    break;
+void parseOptions ( int opt, int id, string[] args, File f ) {
+  if ( opt == 0 ) {
+    f = File( "tasks", "r" );
+    int lineNumber = 1;
+    writeln( "\nID  Task" );
+    writeln( "--------" );
+    while( !f.eof() ){
+      string line = chomp( f.readln() );
+      writef( "%d   %s\n", lineNumber, line );
+      lineNumber++;
+    }
+    f.close();
+  } else if ( opt == 1 ) {
+    f = File( "tasks","a" );
+    for ( int i = 2; i < args.length; i++ ) {
+      f.write( args[i] );
+      f.write( " " );
+    }
+    f.write( "\n" );
+    f.close();
+  } else if ( opt == 2 ) {
+    if ( args.length < 3 ) {
+      writeln( "usage: todo d [ID]" );
+    } else {
+      id = to!int( args[2] );
+      //
+      // Add code to delete task from f
+      //
+      writef( "Deleted task %d\n", id );
+    }
+  } else {
+    writeln( "Unkown argument" );
   }
 }
 
-void printUsage(){
-  writeln("usage: todo <option> [task]");
-}
-
-// Not using
-// --------
-void input(){
-  write("Enter: ");
-  auto inputString = strip(stdin.readln());
-  writeln(inputString);
+int stringToInt( string s ){
+  if ( s == "l" ) { return 0; }
+  else if ( s == "a" ) { return 1; }
+  else if ( s == "d" ) { return 2; }
+  else { return -1; }
 }
 
