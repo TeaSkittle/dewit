9419c1b141780f48721ef004813606ad942870e0
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2021-05-19 10:30:10 -0600

    Started delete option

--- main.d
+++ main.d
@@ -3,6 +3,7 @@ import std.stdio;
 import std.string;
 import std.conv;
 import std.file;
+import std.exception;
 
 int main( string[] args ){
   int id, opt;
@@ -17,37 +18,68 @@ int main( string[] args ){
 }
 
 void parseOptions ( int opt, int id, string[] args, File f ) {
-  if ( opt == 0 ) {
-    f = File( "tasks", "r" );
-    int lineNumber = 1;
-    writeln( "\nID  Task" );
-    writeln( "--------" );
-    while( !f.eof() ){
-      string line = chomp( f.readln() );
-      writef( "%d   %s\n", lineNumber, line );
-      lineNumber++;
+  if ( opt == 0 ) {                              // List tasks
+    if ( exists( "tasks" )) {
+      f = File( "tasks", "r" );
+      int lineNumber = 1;
+      writeln( "\nID  Task" );
+      writeln( "--------" );
+      while( !f.eof() ) {
+        string line = strip( f.readln() );
+        writef( "%d   %s\n", lineNumber, line );
+        lineNumber++;
+        } f.close();
+    } else {
+      writeln( "No tasks in list" );
     }
-    f.close();
-  } else if ( opt == 1 ) {
+  }
+  else if ( opt == 1 ) {                         // Add task
     f = File( "tasks","a" );
     for ( int i = 2; i < args.length; i++ ) {
       f.write( args[i] );
       f.write( " " );
-    }
-    f.write( "\n" );
+    } f.write( "\n" );
     f.close();
-  } else if ( opt == 2 ) {
+  }
+  else if ( opt == 2 ) {                         // Delte a task
     if ( args.length < 3 ) {
       writeln( "usage: todo d [ID]" );
     } else {
       id = to!int( args[2] );
-      //
-      // Add code to delete task from f
-      //
-      writef( "Deleted task %d\n", id );
+      if ( exists( "tasks" )) {
+        int lineNumber = 1;
+        //
+        //File newFile = File( "newTasks", "w" );
+        //
+        // Finally got the task to remove
+        //  TODO - Output to newTasks instead of stdout
+        //       - Remove old file( tasks )
+        //       - Rename newTasks to tasks
+        //       - Add bounds checking
+        // 
+        f = File( "tasks", "r" );
+        while( !f.eof() ) {
+          if ( lineNumber < id ) {
+            string line = strip( f.readln() );
+            writef( "%s\n", line );
+            lineNumber++;
+          } else if ( lineNumber == id ) {
+            string line = strip( f.readln() );
+            //writef( "---%s--\n", line );
+            lineNumber++;
+          } else if ( lineNumber > id ) {
+            string line = strip( f.readln() );
+            writef( "%s\n", line );
+            lineNumber++;
+          } 
+        } f.close();
+        writef( "Deleted task %d\n", id );
+      } else {
+        writeln( "No tasks in list" );
+      }
     }
-  } else {
-    writeln( "Unkown argument" );
+  } else {                                        // Unkwon option
+    writeln( "Error: unkown argument" );
   }
 }
 
